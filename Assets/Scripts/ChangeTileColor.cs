faf72e8324bab4907e9231bb3124b31a4119bc72
regireed89 <regireed89@gmail.com>
2017-10-18 04:23:11 -0500

    fix color

--- Assets/Scripts/ChangeTileColor.cs
+++ Assets/Scripts/ChangeTileColor.cs
@@ -6,12 +6,12 @@ public class ChangeTileColor : MonoBehaviour
 {
     private bool currentTile;
     private bool tagged;
-    GameObject map;
     private void Update()
     {
         if (Input.touchCount == 1)
         {
             RaycastHit hit;
+            
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             if (Physics.Raycast(ray, out hit))
             {
@@ -30,10 +30,5 @@ public class ChangeTileColor : MonoBehaviour
                 }
             }
         }
-        foreach(Transform tile in map.GetComponent<MapGenerator>().tiles)
-        {
-            if(tile.GetComponent<ChangeTileColor>().tagged == true)
-                GetComponent<Renderer>().material.color = Color.green;
-        }
-    }
+     }
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -6,13 +6,10 @@ public class MapGenerator : MonoBehaviour {
 
     public Transform tilePrefab;
     public Vector2 mapSize;
-    [HideInInspector]
-    public List<Transform> tiles;
     [Range(0,1)]
     public float outLinePercent;
     private void Start()
     {
-        GenerateMap();
     }
 
     public void GenerateMap()
@@ -25,16 +22,15 @@ public class MapGenerator : MonoBehaviour {
         }
         Transform mapHolder = new GameObject(holderName).transform;
         mapHolder.parent = transform;
-
-        for(int i = 0; i < mapSize.x; i++)
+        for (int i = 0; i < mapSize.x; i++)
         {
             for(int j = 0; j < mapSize.y; j++)
             {
+                
                 Vector3 tilePosition = new Vector3(-mapSize.x/2 + 0.5f + i, 0, -mapSize.y / 2 + 0.5f + j);
                 Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
                 newTile.localScale = Vector3.one * (1 - outLinePercent);
                 newTile.parent = mapHolder;
-                tiles.Add(newTile);
             }
         }
     }
