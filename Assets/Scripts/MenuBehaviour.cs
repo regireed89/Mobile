2f082c7355df43e7504eff18e0723146f5c72e25
regireed89 <regireed89@gmail.com>
2017-11-22 01:22:09 -0600

    game menu

---
+++ Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour {
+
+    public Canvas Instructions;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("0.Main");
+    }
+    public void LoadInstructions()
+    {
+        Instructions.gameObject.SetActive(true);
+    }
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+    public void Back()
+    {
+        Instructions.gameObject.SetActive(false);
+    }
+        
+}
