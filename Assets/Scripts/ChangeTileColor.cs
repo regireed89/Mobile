0a5931315c55b5793b2977e7146be0700d01c5b7
regireed89 <regireed89@gmail.com>
2017-11-28 11:30:36 -0600

    k

--- Assets/Scripts/ChangeTileColor.cs
+++ Assets/Scripts/ChangeTileColor.cs
@@ -5,7 +5,6 @@ namespace Tile
 {
     public class ChangeTileColor : MonoBehaviour
     {
-
         public TileData data;
         MapGenerator m;
         private void Start()
@@ -19,31 +18,31 @@ namespace Tile
         {
             if (Input.GetMouseButtonDown(0))
             {
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(ray, out hit))
+                TileChange();  
+            }
+        }
+
+        public void TileChange()
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider == GetComponent<Collider>())
                 {
-                    if (hit.collider == GetComponent<Collider>())
+                    data.currentTile = true;
+                    if (data.currentTile == true)
                     {
-                        //foreach (GameObject g in m.current.GetComponent<TileData>().neighbor)
-                        //    if (this.gameObject != g)
-                        //        return;
-                        m.current = this.gameObject;
-                        data.currentTile = true;
-                        if (data.currentTile == true)
-                        {
-                            data.tagged = true;
-                            GetComponent<Renderer>().material.color = Color.red;
-                        }
-                    }
-                    if (hit.collider != GetComponent<Collider>() && data.tagged == true)
-                    {
-                        data.currentTile = false;
-                        GetComponent<Collider>().enabled = false;
-                        GetComponent<Renderer>().material.color = Color.blue;
+                        data.tagged = true;
+                        GetComponent<Renderer>().material.color = Color.red;
                     }
                 }
-
+                if (hit.collider != GetComponent<Collider>() && data.tagged == true)
+                {
+                    data.currentTile = false;
+                    GetComponent<Collider>().enabled = false;
+                    GetComponent<Renderer>().material.color = Color.blue;
+                }
             }
         }
     }
