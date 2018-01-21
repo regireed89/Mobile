4576868d3bc77a7fc1c4727062a46119360e81ce
regireed89 <regireed89@gmail.com>
2018-01-21 00:21:16 -0600

    flick ball too fast

--- Assets/Scripts/FlickBall.cs
+++ Assets/Scripts/FlickBall.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlickBall : MonoBehaviour {
+public class FlickBall : MonoBehaviour
+{
+    Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    Vector2 startPos;
+    float startTime;
+    Vector2 endPos;
+    float endTIme;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startPos = Input.GetTouch(0).position;
+            startTime = Time.time;
+        }
+        if (Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endPos = Input.GetTouch(0).position;
+            endTIme = Time.time;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-        
+            var dis = startPos - endPos;
+            var time = startTime - endTIme;
 
-	}
+            rb.AddForce(dis/(time/2), 0);
+        }
+    }
 }
