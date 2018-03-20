ea515a2059a7d7e714d899c650a0af85fa0b5b9e
regireed89 <regireed89@gmail.com>
2018-03-20 18:53:42 -0500

    trash

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,21 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class Timer : MonoBehaviour {
+public class Timer : MonoBehaviour
+{
 
     public Text counterText;
     public float seconds, minutes;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         counterText = GetComponent<Text>() as Text;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        minutes = (int)(Time.time / 60f);
-        seconds = (int)(Time.time % 60);
-        counterText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("0.Main"))
+        {
+            minutes = (int)(Time.time / 60f);
+            seconds = (int)(Time.time % 60);
+            counterText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+        }
+
+    }
 }
